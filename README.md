981afc4aca24fe30b37093842b76265ffd14421d
ZpCode <zp_code@163.com>
2021-02-03 10:17:53 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,10 @@
+## 简易关键字搜索并展示下拉候选列表插件
+
+**使用流程**
+
+1. 克隆项目
+
+2. 编写搜索请求 api，接收和处理请求方式为 POST，Content-Type 为 application/x-www-form-urlencoded
+	的 kw 参数。记 api 地址为 U。
+
+3. 修改 ./index.html 文件中搜索 button 的 data-url 属性值为 U。
---
+++ assets/js/dropdownsearch.js
@@ -0,0 +1,97 @@
+$(function () {
+    // 隐藏元素用的 class 名称
+    let hiddenClass = 'hidden';
+
+    /**
+     * 判断搜索结果选择列表是否为显示状态
+     *
+     * @returns {boolean}
+     */
+    function isSearchCandidatesShow() {
+        return -1 == $('#searchCandidates').attr('class').indexOf(hiddenClass)
+    }
+
+    /**
+     * 隐藏搜索结果选择列表
+     */
+    function hideCandidates() {
+        if (isSearchCandidatesShow()) {
+            $('#searchCandidates').addClass(hiddenClass)
+        }
+    }
+
+    /**
+     * 显示搜索结果选择列表
+     */
+    function showCandidates() {
+        if (false == isSearchCandidatesShow()) {
+            $('#searchCandidates').removeClass(hiddenClass)
+        }
+    }
+
+    /**
+     * 渲染搜索结果选择列表
+     *
+     * @param candidates
+     */
+    function enrichCandidates(candidates) {
+        let htmlStr = ``;
+        for (let i = 0; i < candidates.length; i++) {
+            htmlStr += `<li data-id="` + candidates[i].id + `">` + candidates[i].name + `</li>`;
+        }
+        $('#searchCandidates ul').empty().append(htmlStr);
+        showCandidates()
+    }
+
+    /**
+     * 注册搜索结果条目中的点击事件
+     */
+    function doCandidate() {
+        $('#searchCandidates ul li').each((index, element) => {
+            let candidate = $(element);
+            candidate.click(() => {
+                $('#searchId').val(candidate.attr('data-id'));
+                $('#searchKw').val(candidate.text());
+                hideCandidates()
+            })
+        })
+    }
+
+    /**
+     * 定义关键字搜索按钮点击事件
+     */
+    $('#searchButton').click(function () {
+        let kw = $('#searchKw').val().trim();
+        if ('' == kw) return false;
+
+        // 搜索请求 api 地址
+        let urlForSearch = $(this).attr('data-url');
+        // support demo code
+        if ('url for search' == urlForSearch) {
+            let data = [
+                {id: 1, name: '搜索结果示例条目一'},
+                {id: 2, name: '搜索结果示例条目二'},
+                {id: 3, name: '搜索结果示例条目三'},
+                {id: 4, name: '搜索结果示例条目四'},
+                {id: 5, name: '搜索结果示例条目五'}
+            ];
+            enrichCandidates(data);
+            doCandidate()
+        } else {
+            $.post(urlForSearch, {kw: kw}, function (res) {
+                if (null != res.data) {
+                    let data = res.data;
+                    enrichCandidates(data);
+                    doCandidate()
+                }
+            })
+        }
+    });
+
+    /**
+     * 定义关键字输入框的聚焦事件
+     */
+    $('#searchKw').focus(function () {
+        hideCandidates()
+    });
+});
